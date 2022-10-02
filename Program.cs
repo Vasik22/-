ccc28565003071c041a5cf67239e4f877c9824bd
Your Name <you@example.com>
2022-10-02 21:35:02 +0300

    program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Задача :
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Описание решения задачи: 
+
+ 
+Console.WriteLine(" Введите массив строк через пробел ");
+string arr = Console.ReadLine();
+string[] Q = arr.Split(' ');
+
+var result = new string[Q .Length];
+            var realSize = 0;
+            foreach (var value in Q)
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                   
+                }
+            }
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
